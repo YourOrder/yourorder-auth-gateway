dcdfc4b32b10e83c11511c9745cc0dc20a8a6ea5
The_JCN <caxashuklin@mail.ru>
2026-03-02 23:46:53 +0500

    init commit

---
+++ src/test/java/jcn/yourorder/authgateway/AuthGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package jcn.yourorder.authgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
