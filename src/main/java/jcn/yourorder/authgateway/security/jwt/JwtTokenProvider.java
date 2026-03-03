c6e7aab00d8a31473c7a27ec80a24f07a676080d
The_JCN <caxashuklin@mail.ru>
2026-03-03 22:35:57 +0500

    Copy realization of JWT token from Internet

--- src/main/java/jcn/yourorder/authgateway/security/jwt/JwtTokenProvider.java
+++ src/main/java/jcn/yourorder/authgateway/security/jwt/JwtTokenProvider.java
@@ -1,4 +1,42 @@
 package jcn.yourorder.authgateway.security.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jcn.yourorder.authgateway.auth.enitites.models.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
 public class JwtTokenProvider {
+
+    private final RsaKeyProvider rsaKeyProvider;
+
+    public String generateAccessToken(User user) {
+        return Jwts.builder()
+                .subject(user.getId().toString())
+                .claim("username", user.getUsername())
+                .claim("email", user.getEmail())
+                .claim("role", user.getRole())
+                .issuedAt(Date.from(Instant.now()))
+                .expiration(new Date(Instant.now().toEpochMilli() + 15 * 60 * 1000))
+                .signWith(rsaKeyProvider.getPrivateKey(), Jwts.SIG.RS256)
+                .compact();
+    }
+
+    public Claims validateToken(String token) {
+
+        return Jwts.parser()
+                .verifyWith(rsaKeyProvider.getPublicKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public boolean isTokenValid(String token) {
+        return validateToken(token) == null;
+    }
 }
--- src/main/java/jcn/yourorder/authgateway/security/jwt/RsaKeyProvider.java
+++ src/main/java/jcn/yourorder/authgateway/security/jwt/RsaKeyProvider.java
@@ -1,4 +1,32 @@
 package jcn.yourorder.authgateway.security.jwt;
 
+import org.springframework.stereotype.Component;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+@Component
 public class RsaKeyProvider {
+
+    private final KeyPair keyPair;
+
+    public RsaKeyProvider() {
+        try {
+            KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+            generator.initialize(2048);
+            this.keyPair = generator.generateKeyPair();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to generate RSA key pair", e);
+        }
+    }
+
+    public PrivateKey getPrivateKey() {
+        return keyPair.getPrivate();
+    }
+
+    public PublicKey getPublicKey() {
+        return keyPair.getPublic();
+    }
 }
